04deb8e932e0785faf78ae95849025dfa3f2eedb
rodgh522 <rodgh522@gmail.com>
2021-07-16 12:35:29 +0900

    ios modified

--- social-login.ios.js
+++ social-login.ios.js
@@ -1,10 +1,9 @@
 import { Common } from './social-login.common';
-
+import { Application } from '@nativescript/core';
 export class SocialLogin extends Common {
     constructor() { 
         super();
     }
-
 
     init(){}
 }
