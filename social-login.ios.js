f4639473f5282980d13029b17256451bb619d37d
rodgh522 <rodgh522@gmail.com>
2021-07-16 12:41:25 +0900

    conflict

--- social-login.ios.js
+++ social-login.ios.js
@@ -1,8 +1,24 @@
-import { Common } from './social-login.common';
+import { Common } from 'nativescript-google-login/social-login.common';
+import { Application,  } from '@nativescript/core';
 export class SocialLogin extends Common {
     constructor() { 
         super();
+
+        this.config = {};
     }
 
-    init(){}
+    init(option){
+        this.config = this.defaultConfig;
+        this.signInConfig = GIDConfiguration.alloc().initWithClientID(option.clientId);
+
+        GIDSignIn.sharedInstance.restorePreviousSignInWithCallback(option.callback);
+    }
+    
+    login(callback){
+        GIDSignIn.sharedInstance.signInWithConfigurationPresentingViewControllerCallback(this.signInConfig, Application.ios.rootController, callback);
+    }
+
+    logout(){
+        GIDSignIn.sharedInstance.signOut();
+    }
 }
